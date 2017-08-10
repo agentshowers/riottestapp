354bd958c699e1eab28e077103f774e69a16986a
Joao Amorim <joao.amorim@outsystems.com>
2017-08-11 00:14:27 +0100

    Front-end improvements (+ removed key)

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import os
 
 def load_from_env():
     """Loads the configuration variables from the enviroment."""
-    config = {"API_KEY" : os.getenv("API_KEY", "RGAPI-008f6402-5ce2-4f9a-96ec-b73d5a8e6737"),
+    config = {"API_KEY" : os.getenv("API_KEY"),
               "PORT" : os.getenv("PORT", 4555),
               "HOST" : os.getenv("HOST", "0.0.0.0")}
     return config
--- src/static/script.js
+++ src/static/script.js
@@ -19,7 +19,7 @@ function getData(s, r) {
             region: r
         },
         success: function(data) {
-            displayResult(JSON.parse(data))      
+            displayResult(data)      
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
             displayError(XMLHttpRequest)
@@ -28,33 +28,32 @@ function getData(s, r) {
 }
 
 function displayResult(data) {
-    $("#gameID").html("Game ID: " + data.gameId)
     var tableHtml = headerRow();
     for (var i = 0; i < data.participants.length; i++) { 
         tableHtml += createRow(data.participants[i]);
     }
-    $("#participantTable").html(tableHtml)
-    $("#errorMessage").html("")
+    $("#participantTable").html(tableHtml);
+    $("#errorMessage").html("");
+    $("#gamedata").css("display","");
 }
 
 function headerRow () {
-    return "<tr><th>Summoner ID</th><th>Summoner Name</th> <th>Team ID</th><th>Champion ID</th><th>Champion Name</th><th>Champion Mastery</th></tr>"
+    return "<tr><th>Summoner ID</th><th>Summoner Name</th> <th>Team ID</th><th>Champion ID</th><th>Champion Name</th><th>Champion Mastery</th></tr>";
 }
 
 function createRow(participant) {
-    var tr = '<tr>' ;
-    tr += '<td>' + participant["summonerId"]  + '</td>';
-    tr += '<td>' + participant["summonerName"]  + '</td>';
-    tr += '<td>' + participant["teamId"]  + '</td>';
-    tr += '<td>' + participant["championId"]  + '</td>';
-    tr += '<td>' + participant["championName"]  + '</td>';
-    tr += '<td>' + participant["championMastery"]  + '</td>';
-    tr +='</tr>';
+    var tr = "<tr>";
+    tr += "<td>" + participant["summonerId"]  + "</td>";
+    tr += "<td>" + participant["summonerName"]  + "</td>";
+    tr += "<td>" + participant["teamId"]  + "</td>";
+    tr += "<td>" + participant["championId"]  + "</td>";
+    tr += "<td>" + participant["championName"]  + "</td>";
+    tr += "<td>" + participant["championMastery"]  + "</td>";
+    tr +="</tr>";
     return tr;
 }
 
 function displayError(response){
-    $("#gameID").html("");
-    $("#participantTable").html("");
     $("#errorMessage").html("An error occured: " + response.responseText + "<br/> Status Code: " + response.status);
+    $("#gamedata").css("display","none");
 }
